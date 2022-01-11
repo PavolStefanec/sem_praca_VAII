1326aa477f6e9a5627dc520c744c8612daa99386
pavol stefanec <palo.stefanec@hotmail.com>
2022-01-11 17:38:31 +0200

    responsiveness works now (kinda)

--- App/Views/Home/index.view.php
+++ App/Views/Home/index.view.php
@@ -12,28 +12,46 @@
     </div>
 </div>
 
-<section class="news">
-    <h1>News</h1>
-    <p>Updates every day</p>
+<section>
+    <div class="news">
+        <h1>News</h1>
+        <p>Updates every day</p>
+    </div>
     <?php
     $counter = 0;
     for ($i = 0; $i < count($data['news']); $i++) {
         if ($counter % 3 == 0) {?>
             <div class="row">
         <?php } ?>
-            <div class="news-col">
-                <?php if ($counter == 0 && App\Auth::isLogged()) {?>
-                    <a href="?c=news&a=newsForm"><i class="fas fa-plus-circle"></i></a>
-                    <?php $i--;?>
-                <?php } else {
-                    $news = $data['news'][$i]?>
-                    <h3> <?= $news->getTitle() ?></h3>
-                    <p> <?= $news->getContent() ?></p>
-                    <?php if (App\Auth::isLogged()) {?>
-                        <a href="?c=news&a=modifyForm&id=<?= $news->getId() ?>"><i class="fas fa-cog" ></i></a>
-                        <a href="?c=news&a=deleteNews&id=<?= $news->getId() ?>"><i class="fas fa-minus-circle"></i></a>
-                    <?php } ?>
-                <?php } ?>
+            <div class="col-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="card news-col">
+                    <div class="card-body">
+                        <?php if ($counter == 0 && App\Auth::isLogged()) {?>
+                            <div class="plus">
+                                <a class="addIcon" href="?c=news&a=newsForm"><i class="fas fa-plus-circle"></i></a>
+                            </div>
+                            <?php $i--;?>
+                        <?php } else {
+                            $news = $data['news'][$i]?>
+                            <div class="cardIcons">
+                                <?php if (App\Auth::isLogged()) {?>
+                                    <div class = "row">
+                                        <div class="col-6 col-sm-6 col-md-6 col-lg-6 cog">
+                                            <a href="?c=news&a=modifyForm&id=<?= $news->getId() ?>"><i class="fas fa-cog" ></i></a>
+                                        </div>
+                                        <div class="col-6 col-sm-6 col-md-6 col-lg-6 minus">
+                                            <a href="?c=news&a=deleteNews&id=<?= $news->getId() ?>"><i class="fas fa-minus-circle"></i></a>
+                                        </div>
+                                    </div>
+                                <?php } ?>
+                            </div>
+                            <div class="cardText">
+                                <h3 class="card-title"> <?= $news->getTitle() ?></h3>
+                                <p class="card-text"> <?= $news->getContent() ?></p>
+                            </div>
+                        <?php } ?>
+                    </div>
+                </div>
             </div>
         <?php if ($counter % 3 == 2) { ?>
             </div>
--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,10 +1,12 @@
 // let navLinks = document.getElementById("navLinks");
 //
 // function showNav(){
-//     navLinks.style.right = "0";
+//     // navLinks.style.right = "0";
+//     navLinks.style.visibility = "visible";
 // }
 // function hideNav(){
-//     navLinks.style.right = "-200px";
+//     // navLinks.style.right = "-200px";
+//     navLinks.style.visibility = "hidden";
 // }
 
 
@@ -23,23 +25,34 @@ function getAllBands () {
                 if (counter % 3 == 0)
                     html += '<div class="row">';
 
-                html += '<div class="bands-col">';
+                html += '<div class="col-12 col-sm-12 col-md-6 col-lg-4"> ' +
+                    '<div class="card bands-col">' +
+                    '<div class="card-body">';
                 if (counter == 0 && data[1]) {
-                    html += '<a href="?c=band&a=bandForm"><i class="fas fa-plus-circle"></i></a>';
+                    html += '<div class="plus-b">' +
+                        '<a class="addIcon" href="?c=band&a=bandForm"><i class="fas fa-plus-circle"></i></a>' +
+                        '</div>';
                     i--;
                 } else {
                     let band = data[0][i];
+                    html += '<div class="cardIcons">';
+                    if (data[1]) {
+                        html += '<div class="row"> ' +
+                            '<div class="col-2 col-sm-2 col-md-2 col-lg-2 cog">' +
+                            '<a href="?c=band&a=modifyForm&id=' + band['id'] + '"><i ' +
+                            'class="fas fa-cog" ></i></a>' +
+                            '</div>' +
+                            '<div class="col-2 col-sm-6 col-md-6 col-lg-2 minus">' +
+                            '<i class="fas fa-minus-circle" onclick="deleteBand(' + band['id'] + ')"></i>' +
+                            '</div> </div>';
+                    }
+                    html += '</div>'
                     html += '<img src="public/files/' + band['logo_src'] + '" ' +
                         'class="img-fluid mx-auto d-block" alt="band1">' +
                         '<a class="nav-link" href="?c=band&a=bandPage&id=' + band['id'] + '"><p>' +
                         band['name'] + '</p></a>';
-                    if (data[1]) {
-                        html += '<a href="?c=band&a=modifyForm&id=' + band['id'] + '"><i ' +
-                            'class="fas fa-cog" ></i></a>' +
-                            '<i class="fas fa-minus-circle" onclick="deleteBand(' + band['id'] + ')"></i>';
-                    }
                 }
-                html += '</div>';
+                html += '</div> </div> </div>';
 
                 if (counter % 3 == 2) {
                     html += '</div>';
